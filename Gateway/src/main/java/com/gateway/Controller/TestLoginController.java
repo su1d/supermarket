5a96fc6edbc7735f4428dc4da66e172125970553
苏国韦 <Arwin.su@farben.com.cn>
2024-07-31 16:01:07 +0800

    增加nacos注册中心

--- Gateway/src/main/java/com/gateway/Controller/TestLoginController.java
+++ Gateway/src/main/java/com/gateway/Controller/TestLoginController.java
@@ -1,5 +1,6 @@
 package com.gateway.Controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/system")
 public class TestLoginController {
 
+    @Value("${spring.cloud.nacos.discovery.server-addr}")
+    private String add;
+
+    @Value("${server.port}")
+    private String serverAddress;
+
     @PostMapping("/login")
     public String login(String username, String password){
+        System.out.println(add+serverAddress);
         return "成功";
     }
 }
